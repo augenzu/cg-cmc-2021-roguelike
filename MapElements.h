19de7b80d8af5714386a12231c99ad092d8ac907
augenzu <elfokd@gmail.com>
2021-03-04 16:47:20 +0300

    Add MapElement::INVALID to signal invalid coordinates

--- MapElements.h
+++ MapElements.h
@@ -8,18 +8,17 @@
 enum class MapElement
 {
   EMPTY,
-  STRONG_WALL,
-  FRAGILE_WALL,
+  WALL,
   FAKE_WALL,
   FLOOR,
   PLAYER,
-  EXIT
+  EXIT,
+  INVALID  // unexisting map element, is used to signal invalid coordinates
 };
 
 const std::map<char, const MapElement> map_elements{
   { ' ', MapElement::EMPTY },
-  { '#', MapElement::STRONG_WALL },
-  { '?', MapElement::FRAGILE_WALL },
+  { '#', MapElement::WALL },
   { '%', MapElement::FAKE_WALL },
   { '.', MapElement::FLOOR },
   { '@', MapElement::PLAYER },
@@ -28,8 +27,7 @@ const std::map<char, const MapElement> map_elements{
 
 const std::map<MapElement, const Tile> tiles{
   { MapElement::EMPTY, Tile("resources/tiles/empty.png") },
-  { MapElement::STRONG_WALL, Tile("resources/tiles/strong_wall.png") },
-  { MapElement::FRAGILE_WALL, Tile("resources/tiles/fragile_wall.png") },
+  { MapElement::WALL, Tile("resources/tiles/wall.png") },
   { MapElement::FAKE_WALL, Tile("resources/tiles/fake_wall.png") },
   { MapElement::FLOOR, Tile("resources/tiles/floor.png") },
   { MapElement::PLAYER, Tile("resources/tiles/player.png") },
